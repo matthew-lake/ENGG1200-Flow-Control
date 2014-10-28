7a39c752ba07f796f39bbdbfb5f463c6ac54c76c
Matthew Lake <matthew.lake@tehnika.com.au>
2014-10-28 10:22:12 +1000

    A ton of stuff

--- control_system/control_system.ino
+++ control_system/control_system.ino
@@ -1,37 +1,71 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <Adafruit_MAX31855.h>
  
 // Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_A 5
+#define ONE_WIRE_B 6
+#define DO   2
+#define CS   3
+#define CLK  4
+#define ENCODER 9
+#define TARGET_TEMP 10
+#define HOT_FLOW 11
+#define COLD_FLOW 12
+#define SERVO_A 7
+#define SERVO_B 8
+
+#define TARGET_TEMP_A 45
+#define TARGET_TEMP_B 45
+
+Adafruit_MAX31855 thermocouple(CLK, CS, DO);
+
+Servo servoA;
+Servo servoB;
  
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWireA(ONE_WIRE_A);
+OneWire oneWireB(ONE_WIRE_B);
  
 // Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensorsA(&oneWireA);
+DallasTemperature sensorsB(&oneWireB);
 
 void setup(void)
 {
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
 
   // Start up the library
-  sensors.begin();
+  sensorsA.begin();
+  sensorsB.begin();
+  
+  servoA.attach(SERVO_A);
+  servoB.attach(SERVO_B);
 }
  
 void loop(void)
 {
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
-  Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-
-  Serial.print("Temperature for Device 1 is: ");
-  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? 
-    // You can have more than one IC on the same bus. 
-    // 0 refers to the first IC on the wire
+  double targetTemp = digitalRead(TARGET_TEMP) ? TARGET_TEMP_A : TARGET_TEMP_B;
+  boolean encoder = digitalRead(ENCODER);
+  while (!encoder)
+  {
+    encoder = digitalRead(ENCODER);
+    targetTemp = digitalRead(TARGET_TEMP) ? TARGET_TEMP_A : TARGET_TEMP_B;
+    // call sensors.requestTemperatures() to issue a global temperature
+    // request to all devices on the bus
+    sensorsA.requestTemperatures(); // Send the command to get temperatures
+    sensorsB.requestTemperatures(); // Send the command to get temperatures
+    double inTempA = sensorsA.getTempCByIndex(0);
+    double inTempB = sensorsB.getTempCByIndex(0);
+    
+    double c = thermocouple.readCelsius();
+     if (!isnan(c)) {
+       double outTemp = c;
+     }
+  }
 }
 
